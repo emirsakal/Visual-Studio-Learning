ad93db38e900251b78cdcaea1ac40590308cbc96
Emirhan Furkan Sakal <emirhanfurkansakal@hotmail.com>
2021-10-19 03:09:55 +0300

    03:09

--- Application Two/Application Two/Form1.Designer.cs
+++ Application Two/Application Two/Form1.Designer.cs
@@ -33,13 +33,23 @@ namespace Application_Two
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(138, 207);
+            this.label1.Location = new System.Drawing.Point(46, 46);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(83, 36);
             this.label1.TabIndex = 0;
@@ -62,7 +72,7 @@ namespace Application_Two
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(378, 207);
+            this.label2.Location = new System.Drawing.Point(184, 46);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(83, 36);
             this.label2.TabIndex = 2;
@@ -72,11 +82,108 @@ namespace Application_Two
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(617, 207);
+            this.label3.Location = new System.Drawing.Point(314, 46);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 36);
             this.label3.TabIndex = 3;
             this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(430, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 36);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(540, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 36);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(662, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 36);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "label6";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(314, 270);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 23);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(46, 130);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 36);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(184, 130);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 36);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(314, 130);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(83, 36);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(416, 130);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(97, 36);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(533, 130);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(97, 36);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label12.Location = new System.Drawing.Point(648, 130);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(97, 36);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "label12";
             // 
             // Form1
             // 
@@ -84,6 +191,16 @@ namespace Application_Two
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
@@ -101,6 +218,16 @@ namespace Application_Two
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
     }
 }
 
--- Application Two/Application Two/Form1.cs
+++ Application Two/Application Two/Form1.cs
@@ -19,9 +19,33 @@ namespace Application_Two
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // Dönüşümlerden önce Convert komutu kullanılır. Örneğin: sayi = Convert.ToInt16(textBox1.Text);
             string x = "Selam";
+            int y = 24;
+            double z = 8.9;
+            int sayi = Convert.ToInt16(textBox1.Text); // ToInt16-32-64 arasında aralık farkı var çok büyük sayılarda 32 ve 64 kullanılıyor.
+            // Double bir sayıyı klavyeden almak için Convert komutunu kullandıktan sonra toDouble() komutunu kullanıyoruz.
+            char harf = 'A'; // Klavyeden alınırken toChar() kullanılır.
+            byte sayicik = 124; // 0-255 arası tüm sayılar verilebilir.
+            sbyte sayi1 = -128, sayi2 = 127; // -128 ile 127 arasında tutabilen byte'ın bir türü.
+            short shortsayi = -32768; // Short -32768 ile +32767 arasındaki sayıları alabilir. O sayılar da dahil.
+            ushort ushortsayi = 65535; // 0 ila 63535 arasındaki sayıları alır.
+            float floatsayi = 2.5f; // Double ve int'in birleşimi gibidir. Double'dan daha küçük bir sayı aralığı vardır. Sonuna 'f' koymak gereklidir.
+            decimal ondaliklisayi = 19.1923m; // Ondalıklı sayıların babasıdır. Double ve Float'tan çok daha yüksek (29 basamak) bir sayı aralığına sahiptir. SONUNA 'm' KOYULMALIDIR.
+            bool durum = false; // bool kontrol = sayi > 20 gibi if ifadelerinde kullanılabilir. True - False olarak çıkar.
 
             label1.Text = x;
+            label2.Text = y.ToString();
+            label3.Text = z.ToString();
+            label4.Text = sayi.ToString();
+            label5.Text = harf.ToString();
+            label6.Text = sayicik.ToString();
+            label7.Text = (sayi1.ToString() + " " + sayi2.ToString());
+            label8.Text = shortsayi.ToString();
+            label9.Text = ushortsayi.ToString();
+            label10.Text = floatsayi.ToString();
+            label11.Text = ondaliklisayi.ToString();
+            label12.Text = durum.ToString();
         }
     }
 }
